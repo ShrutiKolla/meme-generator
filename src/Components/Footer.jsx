67d0da2530e1a92ca048bdebe103aaf6a04e807f
me-mystic <Shrutikolla.2002@gmail.com>
2023-08-18 19:38:09 +0530

    added footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-// imgs
+import trollFace from '../assets/imgs/footerFace.png';
 
 // css
+// import navCss from '../assets/css/nav.module.css';
+import footerCss from '../assets/css/footer.module.css';
 export default function Nav() {
     return(
-        <nav>
-        </nav>
+        <footer>
+            <p> <img src={trollFace} alt="" /> &copy; 2023 Shruti Kolla</p>
+        </footer>
     );
 }
