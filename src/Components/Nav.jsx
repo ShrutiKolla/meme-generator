d2891d310726fbfda7754a955121103bd02cca44
me-mystic <Shrutikolla.2002@gmail.com>
2023-08-18 19:32:45 +0530

    .

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
 // imgs
-import troll from '../assets/imgs/troll-face.svg';
-
+import troll from "../assets/imgs/Troll-Face.svg"
 // css
 import navCss from '../assets/css/nav.module.css';
 export default function Nav() {
