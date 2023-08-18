3bd5f353f70a42d9a6a89ca749afc987229e1b7d
me-mystic <Shrutikolla.2002@gmail.com>
2023-08-18 19:18:42 +0530

    Revert "nav icon removed"
    This reverts commit 6909c72e6e0259fcdc303b441341e8cdc259395b.

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -8,7 +8,7 @@ import navCss from '../assets/css/nav.module.css';
 export default function Nav() {
     return(
         <nav>
-            {/* <img src={troll}/> */}
+            <img src={troll}/>
             <h1 className={navCss.nav__title}>Meme Generator</h1>
             {/* <p>React Course - Project 3</p  > */}
         </nav>
